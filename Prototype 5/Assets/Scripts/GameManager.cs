5bf2cc11048584f5bf2fb57c100d9a335d6b54a6
AlanSeyfir <alan.garcia.nunez@gmail.com>
2021-04-01 17:40:24 -0600

    Protoype 5
    Pracite with the scene manager and UI

---
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartBtn;
+    public bool isGameActive;
+    private int score;
+    private float spawnRate = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameActive = true;
+        score = 0;
+        
+        StartCoroutine("SpawnTargets");
+        UpdateScore(0);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    IEnumerator SpawnTargets()
+    {
+        //If I don't add the while it would spawn one time
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            //If we don't add the Vector and rotation it would spawn weird or in the center.
+            Instantiate(targets[index], Vector3.down, targets[index].transform.rotation);
+        }
+        
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        restartBtn.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        //Get the current scene and assign this method for the button to restart the game/scene 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Vector3 = UnityEngine.Vector3;
+
+public class Target : MonoBehaviour
+{
+    //ALWAYS CLEAN CODE OR COMMENT THE METHODS
+    private Rigidbody objectPrefab;
+    private GameManager gameManager;
+    
+    private float minSpeedUp = 12f;
+    private float maxSpeedUp = 16f;
+    private float maxTorque = 10f;
+    private float xRangePos = 4f;
+    private float yRangePos = -2f;
+
+    public int targetValue;
+    public ParticleSystem explosionParticle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectPrefab = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        
+        //With Vector.up we get rid of the innecessesary 0 in x & z 
+        objectPrefab.AddForce(RandomSpeedSpawn(), ForceMode.Impulse);
+        objectPrefab.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(), ForceMode.Impulse);
+        
+        
+        objectPrefab.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        //It can check when the game is active before it starts doing
+        //all these things to explode all the different objects
+        //With this the player can't click if is gameOver
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(targetValue);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    Vector3 RandomSpeedSpawn()
+    {
+        return Vector3.up * Random.Range(minSpeedUp, maxSpeedUp);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        //No need to add the O in z Bc well it's redundant
+        return new Vector3(Random.Range(-xRangePos, xRangePos), yRangePos);
+    }
+}
