b385e06cffc8190ba5f799ea2f6eee93401c8c27
AlanSeyfir <alan.garcia.nunez@gmail.com>
2021-04-05 15:58:33 -0500

    Refactoring the prototype 1

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    [SerializedField] private Vector3 offset = new Vector3(0, 5, -7);
+    [SerializeField] private Vector3 offset = new Vector3(0, 5, -7);
 
     // Start is called before the first frame update
     void Start()
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     //private variables
-    [SerializedField] private float speed = 10.0f;
+    [SerializeField] private float horsepower = 0f;
+    [SerializeField] private float speed;
     private const float turnSpeed = 40;
     private float horizontalInput;
     private float forwardInput;
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] private TextMeshProUGUI speedText;
+    [SerializeField] private TextMeshProUGUI rpmText;
+    private float rpm;
+    [SerializeField] private List<WheelCollider> allWheels;
+    [SerializeField] private int wheelsOnGround;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
     }
 
     // Update is called once per frame
@@ -25,11 +37,48 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        //We move the vehicle forward - Is the same but more easy to read
-          //transform.Translate(0, 0, 1);
-          //transform.Translate(0,0,1 * Time.deltaTime * 10);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        //We rotate the vehicle
-        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        if (isOnGround())
+        {
+            //We move the vehicle forward - Is the same but more easy to read
+                //transform.Translate(0, 0, 1);
+                //transform.Translate(0,0,1 * Time.deltaTime * 10);
+            //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+            playerRb.AddForce(Vector3.forward * horsepower * forwardInput);
+
+            speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 3.6f); //For Mph/h change 3.6 to 2.237
+            speedText.text = "Speed: " + speed + "km/h";
+            //speedText.SetText("Speed: " + speed + "km/h"); For some reason is better the .text
+
+            rpm = (speed % 30) * 40;
+            rpmText.text = "Rpm: " + rpm;
+        
+            //We rotate the vehicle
+            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        }
+        
     }
+
+    bool isOnGround()
+    {
+        wheelsOnGround = 0;
+        //Understand foreach bc is confusing :/
+        foreach (WheelCollider wheels in allWheels)
+        {
+            if (wheels.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+    
 }
