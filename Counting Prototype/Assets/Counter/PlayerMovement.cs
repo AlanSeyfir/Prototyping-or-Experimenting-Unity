ad03d0bcffb79693d170c66c18f843fc0b90b1a4
AlanSeyfir <alan.garcia.nunez@gmail.com>
2021-04-11 19:32:40 -0500

    Two last challenges of the mission
    Debugging and making a prototype

---
+++ Counting Prototype/Assets/Counter/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Limit zBound = 30
+    public bool reachBoundLimit = false;
+    public int registerKeyPressed = 0;
+    private float zBoundLimit = 25f;
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && registerKeyPressed < 2)
+        {
+            playerRb.AddForce(new Vector3(0,5,2), ForceMode.Impulse);
+            //registerKeyPressed++;
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        float xGoal = 0.0383f;
+        float yGoal = 1.5991f;
+        float zGoal = 0.0303f;
+        Vector3 goalCollider = new Vector3(xGoal, yGoal, zGoal);
+        
+        if (playerRb == true)
+        {
+            Instantiate(playerRb, new Vector3(0.75f, 0.66f, -17.47f), playerRb.transform.rotation);
+            
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (gameObject.transform.position.z > zBoundLimit)
+        {
+            transform.position = new Vector3(0.75f, 0.66f, -17.47f);
+            playerRb.velocity = Vector3.zero;
+            reachBoundLimit = true;
+            Debug.Log(reachBoundLimit);
+        }
+        else
+        {
+            reachBoundLimit = false;
+            Debug.Log(reachBoundLimit);
+        }
+    }
+}
---
+++ Counting Prototype/Assets/Counter/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using Random = UnityEngine.Random;
+
+public class SpawnManager : MonoBehaviour
+{
+    private PlayerMovement playerMovementScript;
+
+    public GameObject[] playerPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnPlayer()
+    {
+        int playerIndex = Random.Range(0, playerPrefab.Length);
+        playerMovementScript = GameObject.Find("SpawnManager").GetComponent<PlayerMovement>();
+        Instantiate(playerPrefab[playerIndex], new Vector3(0.75f, 0.66f, -17.47f), playerPrefab[playerIndex].transform.rotation);
+    }
+    
+}
